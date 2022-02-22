43f9a77232e6482b8ce91e07ab4f784b4de659eb
Amir Shakya <amir.shakya@diagonaltechnologies.com>
2022-02-22 09:53:14 +0545

    Minio config comment added

--- config/minio.php
+++ config/minio.php
@@ -1,6 +1,9 @@
 <?php
 return [
-    'url'          => env('MINIO_URL','https://api.minio.diagonal.solutions/api/v1/upload/'),
+    'url'          => env('MINIO_URL','https://api.minio.diagonal.solutions/api/v1/upload/'), // Minio base url
+    /*
+     * Minio bucket configuration
+     */
     'buckets'      => [
         'default'   => 'ptvimages'
     ]
